4069a7cb0329dc9e1853c13501110b103fe2f499
gergely.papp <papp.gergely012@gmail.com>
2022-04-20 16:10:29 +0200

    adding mui incase if it's needed, axios, first call (not tested), styles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,18 +1,33 @@
+import axios from "axios";
+
 const LoginForm = () => {
-    const handleSubmit = (ev) => {
+
+    const handleSubmit = async (ev) => {
         ev.preventDefault();
-        window.location.href = '/list'
+        const username = ev.target[1].value;
+        const password = ev.target[1].value;
+
+        if (!username || !password) {
+            return alert('Please fill the email and password fields!')
+        }
+
+        const resp = await axios.post('localhost:8008/', {
+            userName: username,
+            password
+        });
+
+        console.log(resp);
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div className="input-container">
-                    <input placeholder="E-mail" />
+                    <input name="email" placeholder="E-mail" />
                 </div>
 
                 <div className="input-container">
-                    <input placeholder="Password" />
+                    <input name="password" placeholder="Password" />
                 </div>
 
                 <button type="submit" className="submit-btn">Login</button>
