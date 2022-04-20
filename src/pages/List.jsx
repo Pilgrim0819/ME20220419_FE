bdc68967b6bf9e67d40bbe5c7cff7d6ef71a6c81
gergely.papp <papp.gergely012@gmail.com>
2022-04-20 21:11:29 +0200

    prettier

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -6,34 +6,34 @@ import getToken from '../utils/getToken';
 import '../styles/List.scss';
 
 const List = () => {
-    const [data, setData] = useState([]);
-    const navigate = useNavigate();
-    const token = getToken();
+  const [data, setData] = useState([]);
+  const navigate = useNavigate();
+  const token = getToken();
 
-    useEffect(() => {
-        if (!token) {
-            navigate('/');
-        }
-
-        return;
-    })
-
-    useEffect(() => {
-        fetchData();
-    }, [])
-
-    const fetchData = async () => {
-      const resp = await axios.get(`${process.env.REACT_APP_API_HOST}/todos`, {
-          headers: {
-              'Access-Control-Allow-Origin': '*',
-              'Authorization': `Bearer ${token}`
-          }
-      })
-
-      if (resp.status === 200) {
-          setData(resp.data)
-      }
+  useEffect(() => {
+    if (!token) {
+      navigate('/');
     }
+
+    return;
+  });
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const resp = await axios.get(`${process.env.REACT_APP_API_HOST}/todos`, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (resp.status === 200) {
+      setData(resp.data);
+    }
+  };
 
   return (
     <div className="list">
